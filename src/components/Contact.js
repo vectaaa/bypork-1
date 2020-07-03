2247c864b98da39346e7834c077c944ebacdac7a
ayodeleoniosun <heywire4u@gmail.com>
2020-07-03 17:13:55 +0100

    Send mails through contact us page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,9 +9,61 @@ import { serverEndPoint, webTitle } from '../utils/Helpers';
 export default function Contact() {
     
 	const history = useHistory();
-	
+    
+    const [values, setValues] = React.useState({
+        fullname: '',
+        subject: '',
+        email: '',
+        message: '',
+    });
+
+    function onChangeHandler(event) {
+        const { name, value } = event.target;
+        setValues(values => ({...values, [name] : value}));
+    }
+
+    async function sendMessage(event) {
+    
+        event.preventDefault();
+        const status = document.getElementById('contact-status');
+        const btn = document.getElementById('contact-btn');
+          
+        const { email, subject, fullname, message } = values;        
+        const allValues = [email, subject, fullname, message];
+        const isValueEmpty = Functions.isEmpty(allValues);
+
+        if(isValueEmpty) {
+            status.innerHTML = "<p style='color:red'> Fill all fields </p>";
+            return false;
+        } else {
+            
+            Functions.disableBtn(btn);
+            const data = `fullname=${fullname}&subject=${subject}&email=${email}&message=${message}`; 
+            
+            axios({
+                method: 'post',
+                url: `${serverEndPoint}contact-us`,
+                data: data,
+            })
+            .then( async (response) => {
+                Functions.enableBtn('Send Message', btn);
+                
+                if(response.status == "200") {
+                    status.innerHTML = `<p style='color:green'>Message sent. We shall get back to you shortly</p>`;
+                    document.getElementById('contact-form').reset();
+                } else {
+                    status.innerHTML = `<p style='color:red'>Message not sent. Try again.</p>`;
+                }
+            })
+            .catch( (error) => {
+                Functions.enableBtn('Send message', btn);
+                status.innerHTML = "<p style='color:red'>Message not sent. Try again.</p>";
+            });
+        }     
+    }
+
 	useEffect( () => {
-		document.title = webTitle+" Contact us";
+		document.title = webTitle+" Contact Us";
     }, []);
 	
     return (
@@ -107,43 +159,40 @@ export default function Contact() {
                                     <span></span>
                                 </div>
                             </div>
-                            <p>Our Team is happy to answer your Questions. Fill out the form, and we’ll be in touch as soon as Possible.</p>
+                            <p>Our Team is happy to answer your questions. Fill out the form, and we’ll be in touch as soon as possible.</p>
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="spacing-6 ttm-bgcolor-grey mt-0 mb-0">
-                                        <form id="ttm-quote-form" class="row ttm-quote-form clearfix" method="post" action="#">
+                                        <form id="contact-form" class="row ttm-quote-form clearfix" method="post" action="#">
                                             <div class="col-sm-6 col-md-6">
                                                 <div class="form-group">
-                                                    <input name="name" type="text" class="form-control with-border bg-white" placeholder="Full Name*" required="required"/>
+                                                    <input type="text" class="form-control with-border bg-white" name="fullname" placeholder="Full Name" required="required"  onChange={onChangeHandler}/>
                                                 </div>
                                             </div>
                                             <div class="col-sm-6 col-md-6">
                                                 <div class="form-group">
-                                                    <input name="phone" type="text" placeholder="Phone Number*" required="required" class="form-control with-border bg-white"/>
+                                                    <input name="email" type="email" placeholder="Email Address" required="required" class="form-control with-border bg-white" onChange={onChangeHandler}/>
                                                 </div>
                                             </div>
-                                            <div class="col-sm-6 col-md-6">
+                                            <div class="col-sm-12 col-md-12">
                                                 <div class="form-group">
-                                                    <input name="address" type="text" placeholder="Email Address*" required="required" class="form-control with-border bg-white"/>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-6 col-md-6">
-                                                <div class="form-group">
-                                                    <input name="subject" type="text" placeholder="Subject" required="required" class="form-control with-border bg-white"/>
+                                                    <input name="subject" type="text" placeholder="Subject" required="required" class="form-control with-border bg-white" onChange={onChangeHandler}/>
                                                 </div>
                                             </div>
 
                                             <div class="col-sm-12 col-md-12">
                                                 <div class="form-group">
-                                                    <textarea name="Message" rows="4" placeholder="Write A Message..." required="required" class="form-control with-border bg-white"></textarea>
+                                                    <textarea name="message" rows="7" placeholder="Write A Message" required="required" class="form-control with-border bg-white" onChange={onChangeHandler}></textarea>
                                                 </div>
                                             </div>
                                             <div class="col-md-12">
                                                 <div class="text-left">
-                                                    <button type="submit" id="submit" class="ttm-btn ttm-btn-size-md ttm-btn-bgcolor-darkgrey w-100" value="">
-                                                        Submit 
+                                                    <button type="button" id="contact-btn" onClick={sendMessage} class="ttm-btn ttm-btn-size-md ttm-btn-bgcolor-darkgrey w-100">
+                                                        <i className="fa fa-location-arrow"></i> Send Message
                                                     </button>
                                                 </div>
+                                                <br/>
+                                                <div id="contact-status" align="center"></div>
                                             </div>
                                         </form>
                                     </div>
