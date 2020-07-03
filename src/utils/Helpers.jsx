46cf6beac8e862ac8125930762e1ae6ac924b338
ayodeleoniosun <heywire4u@gmail.com>
2020-07-03 17:28:16 +0100

    Change the database configuration to the production one

--- src/utils/Helpers.jsx
+++ src/utils/Helpers.jsx
@@ -2,6 +2,6 @@ import Functions from './Functions';
 
 export const webTitle = "Bypork | ";
 export const paystackKey = 'pk_test_22dcdb436c42028986efa17b9778de6df2402cdf';
-export const serverEndPoint = 'http://localhost:8000/api/v1/';
+//export const serverEndPoint = 'http://localhost:8000/api/v1/';
 
-//export const serverEndPoint = 'https://server.bineza.com/api/v1/';
+export const serverEndPoint = 'https://server.bypork.com/api/v1/';
